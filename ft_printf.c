741c7bb21f3d8ae3aa09582e3120bada6ed1a88b
rocky <rocky55737@gmail.com>
2022-07-01 22:12:13 +0900

    1.2 concept

--- ft_printf.c
+++ ft_printf.c
@@ -12,11 +12,31 @@ int ft_printf(const char *format, ...)
     return (ret);
 }
 
-int do_printf(va_list arg_ptr, const char *format)
+int do_printf(va_list arg_ptr, const char *fmt)
 {
     int print_cnt;
-    int format_index;
+    int fmt_idx;
+    char    next_chr;
 
     print_cnt = 0;
-    
+    fmt_idx = 0;
+    while (fmt[fmt_idx])
+    {
+        if (fmt[fmt_idx] == '%' && fmt[fmt_idx + 1])
+        {
+            next_chr = fmt[fmt_idx + 1];
+            if (next_chr == 'd' || next_chr == 'i')
+                print_cnt += ft_putnbr(*(int *)arg_ptr);
+            //etc
+            //
+            //
+            //
+            arg_ptr++;
+            fmt_idx++;
+        }
+        else
+            print_cnt += ft_putchr(fmt[fmt_idx]);
+        fmt_idx++;
+    }
+    return (print_cnt);
 }
